a1dce13ed699977b31b9f6ef6b5cfc7c30112d3b
Norbert Nader <nnader@amazon.com>
2022-03-03 08:30:28 -0800

    update after released fix for onSelectionChange prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useRef, useEffect } from 'react'
 import './App.css';
 import { initialize, toId } from '@iot-app-kit/source-iotsitewise';
-import { LineChart, BarChart, ScatterChart, Kpi, StatusTimeline, StatusGrid, ResourceExplorer, WebglContext } from '@iot-app-kit/react-components';
+import { LineChart, BarChart, ScatterChart, Kpi, StatusTimeline, StatusGrid, ResourceExplorer, Table, WebglContext } from '@iot-app-kit/react-components';
 const { defineCustomElements } = require('@iot-app-kit/components/loader');
 defineCustomElements();
 
 function App() {
-  const DEMO_TURBINE_ASSET_1 = '<replace-with-sitwise-asset-id>';
-  const DEMO_TURBINE_ASSET_1_PROPERTY_1 = '<replace-with-sitwise-asset-property-id>';
-  const DEMO_TURBINE_ASSET_1_PROPERTY_2 = '<replace-with-sitwise-asset-property-id>';
+  const DEMO_ASSET_1 = '<replace-with-sitwise-asset-id>';
+  const DEMO_ASSET_1_PROPERTY_1 = '<replace-with-sitwise-asset-property-id>';
+  const DEMO_ASSET_1_PROPERTY_2 = '<replace-with-sitwise-asset-property-id>';
+
+  const DEMO_ASSET_2 = '<replace-with-sitwise-asset-id>';
+  const DEMO_ASSET_2_PROPERTY_1 = '<replace-with-sitwise-asset-property-id>';
+  const DEMO_ASSET_2_PROPERTY_2 = '<replace-with-sitwise-asset-property-id>';
 
   const { query } = initialize({ awsCredentials: {
       "accessKeyId": "<replace-with-aws-access-key-id>",
@@ -16,34 +19,28 @@ function App() {
       "sessionToken": "<replace-with-aws-session-token>",
     }, awsRegion: '<replace-with-aws-region>' });
 
-  const resourceExplorerRef = useRef(null);
-
-  useEffect(() => {
-    resourceExplorerRef.current.onSelectionChange = (event) => console.log('changed asset', event);
-  });
-
   return (
     <div className="App" style={{ width: "1800px", display: "flex" }} >
       <div style={{ width: "400px" }} >
         <ResourceExplorer
-          ref={resourceExplorerRef}
           query={query.assetTree.fromRoot()}
+          onSelectionChange={(event) => console.log('changes asset', event)}
         />
       </div>
       <div style={{ width: "1300px", height: "1600px", display: "flex", flexWrap: "wrap", justifyContent: "space-between" }} >
         <div style={{ width: "400px", height: "400px" }}>
           <LineChart
             widgetId="line-1"
-            settings={{ requestBuffer: 0.2 }}
-            viewport={{ duration: '5m' }}
+            settings={{ requestBuffer: 1 }}
+            viewport={{ duration: '5m', group: 'in-sync' }}
             queries={[
               query.timeSeriesData({
                 assets: [
                   {
-                    assetId: DEMO_TURBINE_ASSET_1,
+                    assetId: DEMO_ASSET_1,
                     properties: [
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_2 },
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_1 },
+                      { propertyId: DEMO_ASSET_1_PROPERTY_2 },
+                      { propertyId: DEMO_ASSET_1_PROPERTY_1 },
                     ],
                   },
                 ],
@@ -56,14 +53,15 @@ function App() {
             widgetId="line-2"
             settings={{ requestBuffer: 0.2 }}
             viewport={{ duration: '5m' }}
+            styleSettings={{ ['testing']: { color: '#0000FF' } }}
             queries={[
               query.timeSeriesData({
                 assets: [
                   {
-                    assetId: DEMO_TURBINE_ASSET_1,
+                    assetId: DEMO_ASSET_1,
                     properties: [
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_2 },
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_1 },
+                      { propertyId: DEMO_ASSET_1_PROPERTY_2, refId: 'testing' },
+                      { propertyId: DEMO_ASSET_1_PROPERTY_1 },
                     ],
                   },
                 ],
@@ -74,16 +72,16 @@ function App() {
         <div style={{ width: "400px", height: "400px" }}>
           <ScatterChart
             widgetId="line-3"
-            settings={{ requestBuffer: 0.2 }}
-            viewport={{ duration: '5m' }}
+            settings={{ requestBuffer: 1 }}
+            viewport={{ duration: '5m', group: 'in-sync' }}
             queries={[
               query.timeSeriesData({
                 assets: [
                   {
-                    assetId: DEMO_TURBINE_ASSET_1,
+                    assetId: DEMO_ASSET_1,
                     properties: [
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_2 },
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_1 },
+                      { propertyId: DEMO_ASSET_1_PROPERTY_2 },
+                      { propertyId: DEMO_ASSET_1_PROPERTY_1 },
                     ],
                   },
                 ],
@@ -96,19 +94,19 @@ function App() {
             widgetId="line-3"
             settings={{ requestBuffer: 0.2  }}
             annotations={{ y: [
-              { color: '#FF0000', comparisonOperator: 'GT', value: 30.45 },
-              { color: '#0000FF', comparisonOperator: 'GT', value: 2.93 },
-              { color: '#00FF00', comparisonOperator: 'LT', value: 2.93 },
+              { color: '#FF0000', comparisonOperator: 'GT', value: 0 },
+              { color: '#0000FF', comparisonOperator: 'GT', value: 1 },
+              { color: '#00FF00', comparisonOperator: 'GT', value: 2 },
               ] }}
             viewport={{ duration: '5m' }}
             queries={[
               query.timeSeriesData({
                 assets: [
                   {
-                    assetId: DEMO_TURBINE_ASSET_1,
+                    assetId: DEMO_ASSET_2,
                     properties: [
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_2 },
-                      { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_1 },
+                      { propertyId: DEMO_ASSET_2_PROPERTY_1 },
+                      { propertyId: DEMO_ASSET_2_PROPERTY_2 },
                     ],
                   },
                 ],
@@ -126,10 +124,10 @@ function App() {
                 query.timeSeriesData({
                   assets: [
                     {
-                      assetId: DEMO_TURBINE_ASSET_1,
+                      assetId: DEMO_ASSET_1,
                       properties: [
-                        { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_2 },
-                        { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_1 },
+                        { propertyId: DEMO_ASSET_1_PROPERTY_2 },
+                        { propertyId: DEMO_ASSET_1_PROPERTY_1 },
                       ],
                     },
                   ],
@@ -143,18 +141,18 @@ function App() {
               settings={{ requestBuffer: 0.2 }}
               viewport={{ duration: '5m' }}
               annotations={{ y: [
-                  { color: '#FF0000', comparisonOperator: 'GT', value: 30.45 },
-                  { color: '#0000FF', comparisonOperator: 'GT', value: 2.93 },
-                  { color: '#00FF00', comparisonOperator: 'LT', value: 2.93 },
+                  { color: '#FF0000', comparisonOperator: 'GT', value: 0 },
+                  { color: '#0000FF', comparisonOperator: 'GT', value: 1 },
+                  { color: '#00FF00', comparisonOperator: 'GT', value: 2 },
                 ] }}
               queries={[
                 query.timeSeriesData({
                   assets: [
                     {
-                      assetId: DEMO_TURBINE_ASSET_1,
+                      assetId: DEMO_ASSET_2,
                       properties: [
-                        { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_2 },
-                        { propertyId: DEMO_TURBINE_ASSET_1_PROPERTY_1 },
+                        { propertyId: DEMO_ASSET_2_PROPERTY_1 },
+                        { propertyId: DEMO_ASSET_2_PROPERTY_2 },
                       ],
                     },
                   ],
@@ -162,6 +160,33 @@ function App() {
               ]}
             />
           </div>
+        </div>
+        <div style={{ width: "400px", height: "400px" }}>
+          <Table
+            widgetId="scatter-1"
+            tableColumns={[
+              { header: 'OEE',
+                rows: [
+                  toId({ assetId: DEMO_ASSET_1, propertyId: DEMO_ASSET_1_PROPERTY_2 }),
+                  toId({ assetId: DEMO_ASSET_1, propertyId: DEMO_ASSET_1_PROPERTY_1 }),
+                ]
+              }
+            ]}
+            viewport={{ duration: '5m', yMin: 0, yMax: 1 }}
+            queries={[
+              query.timeSeriesData({
+                assets: [
+                  {
+                    assetId: DEMO_ASSET_1,
+                    properties: [
+                      { propertyId: DEMO_ASSET_1_PROPERTY_2 },
+                      { propertyId: DEMO_ASSET_1_PROPERTY_1 },
+                    ],
+                  },
+                ],
+              }),
+            ]}
+          />
         </div>
       </div>
       <WebglContext />
